e9717a7927e7f88fe9137481875dbb3bf1bc2679
Christian <yearofthebenji@gmail.com>
2024-07-25 19:00:43 -0500

    .ENV

--- serve.js
+++ serve.js
@@ -1,8 +1,11 @@
 const express = require('espress');
+const sequelize =require('/config/connection')
 
 const app = express();
 const PORT = process.env.port || 3001;
 
+sequelize.sync().then(() => {
 app.listen(PORT, () => {
   console.log('server started!');
 })
+});
