7b77abcee0b84f8d8d8d90b42c0bef532222cd0b
Christian <yearofthebenji@gmail.com>
2024-08-03 00:11:09 -0500

    working on handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,23 @@
 const router = require('express').Router();
 const api = require('./api');
+const Todo = required('../models/Task');
 
 //localhost:3001/api
 router.use("/api", api);
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
+    const allTasks = await Task.findAll();
     res.render('todo');
-})
+    console.log(allTasks);
+    const testdata = {
+        name: 'test work',
+        description: 'work to do',
+        status: 'in progress'
+    };
+    res.render('todo', testData);
+
+
+    })
 
 //localhost:3001/pikachu
 router.get("/pikachu", (req, res) => {
