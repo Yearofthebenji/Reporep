27ae97533d910e717d38a043dcaf23ced930e294
Christian <yearofthebenji@gmail.com>
2024-08-03 22:09:12 -0500

    fixing Nodemod and checking over server.js folder

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
 
